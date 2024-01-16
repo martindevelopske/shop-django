56711b2a87c411b3f40d6cc1b35d6e84e6484e63
Martin Ndung'u <martindevelopske@gmail.com>
2024-01-16 15:04:17 +0300

    almost done

--- item/urls.py
+++ item/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('<int:pk>/', views.detail, name='detail'),
     path('new/', views.new, name='new'),
     path('edit<int:pk>/', views.edit, name="edit"),
-    path('delete<int:pk>/', views.delete, name="delete")
+    path('delete<int:pk>/', views.delete, name="delete"),
+    
 ]
--- item/views.py
+++ item/views.py
@@ -6,6 +6,11 @@ from item.forms import EditItemForm, NewItemForm
 
 from .models import Category, Item
 
+# def browse(request):
+#     items=Item.objects.filter(is_sold=False);
+#     return render(request, 'item/items.html', {"items":items} )
+
+
 def items(request):
     query = request.GET.get('query', '')
     category_id = request.GET.get('category', 0)
